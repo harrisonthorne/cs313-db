a70503e8bc1f4110faa0366722ea7b6cd851bb4a
municorn <municorn@musicaloft.com>
2019-10-19 14:27:11 -0600

    ugh

--- web/api/test.php
+++ web/api/test.php
@@ -1,6 +1,5 @@
 <?php
-require('../main.php');
-header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-header("Access-Control-Allow-*: *");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 echo $_GET['msg'];
 ?>
--- web/main.php
+++ web/main.php
@@ -1,9 +1,4 @@
 <?php
-function cors() {
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
-}
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 ?>
