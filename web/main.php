453e473c338f3a6e9d1afdf3dfaa4fcd5b4343b5
municorn <municorn@musicaloft.com>
2019-10-19 14:33:55 -0600

    UGH

--- web/main.php
+++ web/main.php
@@ -1,4 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 ?>
