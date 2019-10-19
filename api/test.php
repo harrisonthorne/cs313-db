b95a4a029563fca242fb1fa817d410eb28a2bed4
municorn <municorn@musicaloft.com>
2019-10-19 14:43:47 -0600

    what about this?

--- api/test.php
+++ api/test.php
@@ -1,5 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials: true');
+require('../main.php')
 echo $_GET['msg'];
 ?>
--- web/main.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-?>
