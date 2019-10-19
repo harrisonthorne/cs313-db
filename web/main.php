eea38d7ab208a8c37ec7e20967366f8097adf9bd
municorn <municorn@musicaloft.com>
2019-10-19 14:19:47 -0600

    CORS attempt #2

--- web/main.php
+++ web/main.php
@@ -1,3 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+function cors() {
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        /* header('Access-Control-Allow-Credentials: true'); */
+        /* header('Access-Control-Max-Age: 86400');    // cache for 1 day */
+    }
+}
 ?>
