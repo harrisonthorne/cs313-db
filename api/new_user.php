594594cf194ce4e410c14a18a4d474947bbe3d3e
municorn <municorn@musicaloft.com>
2019-10-26 00:27:16 -0600

    Revert "hardcoding keycode for now to avoid openssl errors"
    This reverts commit 3878fca121cd06d7a703405d963900aa069eeb42.

--- api/new_user.php
+++ api/new_user.php
@@ -6,18 +6,18 @@ header('Access-Control-Allow-Methods: POST');
 require('../db.php');
 $db = get_database();
 
-/* function getRandomHex($num_bytes=8) { */
-/*     return bin2hex(openssl_random_pseudo_bytes($num_bytes)); */
-/* } */
+function getRandomHex($num_bytes=8) {
+    return bin2hex(openssl_random_pseudo_bytes($num_bytes));
+}
 
-/* $keycode = getRandomHex(4); */
+$keycode = getRandomHex(4);
 
 // get username and password
 $data = json_decode(file_get_contents('php://input'), true);
 $username = $data['username'];
 $password = $data['password']; // all hail plain text passwords
 
-$search_statement = $db->prepare("INSERT INTO \"public.user\" (id, keycode, username, password) VALUES (nextval('uuid_seq'), '12345678', '$username', '$password');");
+$search_statement = $db->prepare("INSERT INTO \"public.user\" (id, keycode, username, password) VALUES (nextval('uuid_seq'), '$keycode', '$username', '$password');");
 $search_statement->execute();
 $new_id = $db->lastInsertId();
 
