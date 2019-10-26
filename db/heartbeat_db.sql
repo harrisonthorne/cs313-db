cdba44b6c16bc7d5ae0a507f199b97ed46d86fc7
municorn <municorn@musicaloft.com>
2019-10-25 22:44:08 -0600

    update create table scripts

--- db/heartbeat_db.sql
+++ db/heartbeat_db.sql
@@ -1,15 +1,21 @@
-CREATE TABLE IF NOT EXISTS "user" (
-    id                  int         PRIMARY KEY,                -- primary key
-    keycode             varchar(16) UNIQUE,                     -- unique
-    linked              int         REFERENCES user_group (id), -- foreign key (user_group)
+CREATE TABLE IF NOT EXISTS "public.user_group" (
+    id      int     PRIMARY KEY,    -- primary key
+    members int[]                   -- foreign keys (user)
+);
+
+CREATE TABLE IF NOT EXISTS "public.user" (
+    id                  int         PRIMARY KEY,                         -- primary key
+    username            varchar(32),
+    keycode             varchar(8)  UNIQUE,                              -- unique
+    linked              int         REFERENCES "public.user_group" (id), -- foreign key (user_group)
     lastHeartbeatCheck  date
 );
 
-CREATE TABLE IF NOT EXISTS link_request (
-    id          int     PRIMARY KEY,            -- primary key
-    sender      int     REFERENCES "user" (id), -- foreign key (user)
-    recipient   int     REFERENCES "user" (id), -- foreign key (user)
-    status      int,                            -- enum
+CREATE TABLE IF NOT EXISTS "public.link_request" (
+    id          int     PRIMARY KEY,                   -- primary key
+    sender      int     REFERENCES "public.user" (id), -- foreign key (user)
+    recipient   int     REFERENCES "public.user" (id), -- foreign key (user)
+    status      int,                                   -- enum
     send_date   date
 
     -- enum status
@@ -18,15 +24,10 @@ CREATE TABLE IF NOT EXISTS link_request (
     -- 2    Declined
 );
 
-CREATE TABLE IF NOT EXISTS user_group (
-    id      int     PRIMARY KEY,    -- primary key
-    members int[]                   -- foreign keys (user)
-);
-
-CREATE TABLE IF NOT EXISTS heartbeat (
-    id          int PRIMARY KEY,            -- primary key
-    sender      int REFERENCES "user" (id), -- foreign key (user)
-    recipient   int REFERENCES "user" (id), -- foreign key (user)
+CREATE TABLE IF NOT EXISTS "public.heartbeat" (
+    id          int PRIMARY KEY,                   -- primary key
+    sender      int REFERENCES "public.user" (id), -- foreign key (user)
+    recipient   int REFERENCES "public.user" (id), -- foreign key (user)
     count       int,
     send_date   date
 );
